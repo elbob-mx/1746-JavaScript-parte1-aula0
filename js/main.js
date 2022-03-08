8ce0f85c103ce48bb2c2db51354f1ebd6613c0c7
elbob-mx <rob7th@gmail.com>
2022-03-08 11:01:10 -0600

    js-01

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 			var title = document.querySelector(".title"); // selecciona el objeto 'h1'
 			console.log(title); // se muestra el objeto en consola
 			console.log(title.textContent); //  se muetra el contenido del objeto 'title'
-			title.textContent = "Buena Vida Nutrición!";  // se modifica el contenido del objeto por el que se desea
+			title.textContent = "Buena Vida Nutrición";  // se modifica el contenido del objeto por el que se desea
 			console.log(" -> printing from main.js <- ");
