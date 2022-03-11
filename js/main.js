90f21b1a8fefce834322c0d95e11772e0667afa4
elbob-mx <rob7th@gmail.com>
2022-03-10 23:14:56 -0600

    js-02

--- js/main.js
+++ js/main.js
@@ -1,4 +1,32 @@
 //  VARIABLES ------------------------------------------------>
+
+// botón adicionar -------->
+var botonAdicionar = document.querySelector("#adicionar-paciente");
+botonAdicionar.addEventListener("click", (e) => {
+  event.preventDefault();
+  var form = document.querySelector("#form-adicionar");
+
+  var nombre = form.nombre.value;
+  var peso = form.peso.value;
+  var altura = form.altura.value;
+  var gordura = form.gordura.value;
+  
+  console.log(nombre);
+  console.log(peso);
+  console.log(altura);
+  console.log(gordura);
+
+  console.log(" ----> buena adición. ");
+
+  pacienteTr = document.createElement("tr");
+  nombreTd = document.createElement("td");
+  pesoTd = document.createElement("td");
+  alturaTd = document.createElement("td");
+  gorduraTd = document.createElement("td");
+  imcTd = document.createElement("td");
+});
+
+
 var pacientes = document.querySelectorAll(".paciente");
 console.log(pacientes);
 
