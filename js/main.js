8ae93b00a0065791947ecc52936ff42f8c5126c8
elbob-mx <rob7th@gmail.com>
2022-03-10 23:42:12 -0600

    js-02

--- js/main.js
+++ js/main.js
@@ -10,11 +10,15 @@ botonAdicionar.addEventListener("click", (e) => {
   var peso = form.peso.value;
   var altura = form.altura.value;
   var gordura = form.gordura.value;
+
+  var tabla = document.querySelector("#tabla-pacientes");
   
+  /*
   console.log(nombre);
   console.log(peso);
   console.log(altura);
   console.log(gordura);
+  */
 
   console.log(" ----> buena adición. ");
 
@@ -24,6 +28,21 @@ botonAdicionar.addEventListener("click", (e) => {
   alturaTd = document.createElement("td");
   gorduraTd = document.createElement("td");
   imcTd = document.createElement("td");
+
+  nombreTd.textContent = nombre;
+  pesoTd.textContent = peso;
+  alturaTd.textContent = altura;
+  gorduraTd.textContent = gordura;
+
+  pacienteTr.appendChild(nombreTd);
+  pacienteTr.appendChild(pesoTd);
+  pacienteTr.appendChild(alturaTd);
+  pacienteTr.appendChild(gorduraTd);
+
+  console.log(pacienteTr);
+
+  tabla.appendChild(pacienteTr);
+
 });
 
 
