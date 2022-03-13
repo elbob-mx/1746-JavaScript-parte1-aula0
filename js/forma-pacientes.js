d8a1143f6ef2ca867f7098f6f5857cb6d19b3d4e
elbob-mx <rob7th@gmail.com>
2022-03-13 15:32:51 -0600

    js-03

--- js/forma-pacientes.js
+++ js/forma-pacientes.js
@@ -3,33 +3,55 @@ var botonAdicionar = document.querySelector("#adicionar-paciente");
 botonAdicionar.addEventListener("click", (e) => {
   event.preventDefault();
 
-  var form = document.querySelector("#form-adicionar");
+  var form = document.querySelector("#form-adicionar");  
+  var paciente = tomarDatosPacientes(form); // objeto 'paciente' **
+  var pacienteTr = construirTr (paciente); // crea TR del paciente adicionado **
+  var tabla = document.querySelector("#tabla-pacientes"); // declara la variable 'tabla' tomado de la 'tabla-pacientes' **
+  tabla.appendChild(pacienteTr);
 
-  // objeto 'paciente'
-  var paciente = tomarDatosPacientes(form);
-  console.log(paciente);
+});
 
-  // declara la variable 'tabla' tomando de la 'tabla-pacientes'
-  var tabla = document.querySelector("#tabla-pacientes");
+// FUNCIONES *********************************
 
-  console.log(" ----> buena adición. ");
+// función que crea el objeto 'paciente' **
+function tomarDatosPacientes(form) {
+  // toma datos del formulario **
+  var paciente = {
+    nombre: form.nombre.value,
+    peso: form.peso.value,
+    altura: form.altura.value,
+    gordura: form.gordura.value,
+    imc: calcularIMC(form.peso.value, form.altura.value)
+  }
+  return paciente;
+};
 
+function construirTr(paciente) {
   // crea elementos TD y TR ------------------------>
-  pacienteTr = document.createElement("tr");
-  nombreTd = document.createElement("td");
-  pesoTd = document.createElement("td");
-  alturaTd = document.createElement("td");
-  gorduraTd = document.createElement("td");
-  imcTd = document.createElement("td");
+  var pacienteTr = document.createElement("tr");
+  pacienteTr.classList.add("paciente"); // adiciona la clases 'paciente' **
 
+  var nombreTd = document.createElement("td");
+  pacienteTr.classList.add("info-nombre"); // adiciona la clases 'paciente' **
+
+  var pesoTd = document.createElement("td");
+  pacienteTr.classList.add("info-peso"); // adiciona la clases 'paciente' **
+
+  var alturaTd = document.createElement("td");
+  pacienteTr.classList.add("info-altura"); // adiciona la clases 'paciente' **
+
+  var gorduraTd = document.createElement("td");
+  pacienteTr.classList.add("info-gordura"); // adiciona la clases 'paciente' **
+
+  var imcTd = document.createElement("td");
+  pacienteTr.classList.add("info-imc"); // adiciona la clases 'paciente' **
 
   // toma valores con la propiedad textContext y asigna a variables ------------>
   nombreTd.textContent = paciente.nombre;
   pesoTd.textContent = paciente.peso;
   alturaTd.textContent = paciente.altura;
   gorduraTd.textContent = paciente.gordura;
-  imcTd.textContent = calcularIMC(peso, altura);
-
+  imcTd.textContent = paciente.imc;
 
   // asignación al TR 'PACIENTE' de las filas TD ------------>
   pacienteTr.appendChild(nombreTd);
@@ -38,22 +60,5 @@ botonAdicionar.addEventListener("click", (e) => {
   pacienteTr.appendChild(gorduraTd);
   pacienteTr.appendChild(imcTd);
 
-  console.log(pacienteTr);
-
-  tabla.appendChild(pacienteTr);
-
-});
-
-// FUNCIONES *********************************
-
-// función que crea el objeto 'paciente'
-function tomarDatosPacientes(form) {
-  // toma datos del formulario
-  var paciente = {
-    nombre: form.nombre.value,
-    peso: form.peso.value,
-    altura: form.altura.value,
-    gordura: form.gordura.value
-  }
-  return paciente;
-}
+  return pacienteTr;
+};
