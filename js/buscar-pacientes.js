303a2db656d702152ba7498b33f42a614974c32f
elbob-mx <rob7th@gmail.com>
2022-03-20 00:22:55 -0600

    js-04 (ajax)

--- js/buscar-pacientes.js
+++ js/buscar-pacientes.js
@@ -1,5 +1,17 @@
-var btnBuscar = document.querySelector("#boton-paciente");
+var btnBuscar = document.querySelector("#buscar-paciente");
 
 btnBuscar.addEventListener("click", function(){
-  console.log("Pensanding pacientes...");
+  console.log("PENSANDING PACIENTES");
+  var xhr = new XMLHttpRequest;
+  xhr.open("GET", "https://alura-es-cursos.github.io/api-pacientes/pacientes.json");
+  xhr.addEventListener("load", function(){
+    var respuesta = xhr.responseText;
+    console.log(respuesta);
+    console.log(typeof respuesta);
+
+    var pacientes = JSON.parse(respuesta);
+    console.log(pacientes);
+    console.log(typeof pacientes);
+  });
+  xhr.send();
 });
